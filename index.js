4a203cef3b27ad96fdf1f74c9b83cd4e7293ac7d
apon995 <shofikulsamz995@gmail.com>
2023-10-18 22:07:43 +0600

    mongodb-atlas-server-connected

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -11,14 +11,57 @@ app.use(cors());
 app.use(express.json());
 
 
+// --atlas-server-start-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zearr5n.mongodb.net/?retryWrites=true&w=majority`
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+    await client.connect();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch(error){
+    console.log(error)
+  }
+}
+run()
+
+
 
 
 app.get('/', (req, res) => {
-  res.send('Electro server is running !')
+    res.send('Electro server is running !')
 })
 
 app.listen(port, () => {
-  console.log(`Electro sever is running on port ${port}`)
+    console.log(`Electro sever is running on port ${port}`)
 })
 
 
